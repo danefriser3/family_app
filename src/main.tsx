3768a5be4b95b034afee8b5af6d4f1588fe4c245
danefriser3 <147616233+danefriser3@users.noreply.github.com>
2025-09-27 10:18:52 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://localhost:3000/graphql' }), // Cambia se il tuo server è su una porta diversa
+  link: new HttpLink({ uri: 'https://fam-app-be.vercel.app/graphql' }), // Cambia se il tuo server è su una porta diversa
   cache: new InMemoryCache(),
 });
 
